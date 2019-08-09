aa3f85cdb19b6a678931923df83b014be614ce3e
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-09 15:25:43 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -13,34 +13,34 @@ class Person
   end
   
   def happiness=(happiness_level)
-    @happiness = happiness_level
+    self.happiness = happiness_level
     if happiness_level > 10 
-      @happiness = 10
+      self.happiness = 10
       elsif happiness_level < 0
-        @happiness = 0
+        self.happiness = 0
     end
-    return @happiness
+    return self.happiness
   end
   
   def hygiene=(hygiene_level)
-    @hygiene = hygiene_level
+    self.hygiene = hygiene_level
     if hygiene_level > 10
-      @hygiene = 10
+      self.hygiene = 10
       elsif hygiene_level < 0
-      @hygiene = 0
+      self.hygiene = 0
     end
   end
   
   def happy?
-    @happiness > 7 ? true : false
+    self.happiness > 7 ? true : false
   end
   
   def clean?
-    @hygiene > 7 ? true : false
+    self.hygiene > 7 ? true : false
   end
   
   def get_paid(salary)
-    
+    self.bank_account += salary
   end
   
 end
