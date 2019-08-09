4a720a4febd427c1ca3b26552d69faee1e8273b3
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-09 15:15:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,10 +8,11 @@ class Person
   def initialize(name)
     @name = name
     @bank_account = 25
+    @happiness = 8
   end
   
   def happiness_index=(happiness)
-    @happiness = 8
+   
     happiness = @happiness
     if happiness > 10 
       @happiness = 10
